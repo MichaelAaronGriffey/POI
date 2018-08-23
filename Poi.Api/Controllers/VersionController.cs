4978c17f1a0222b7648988fb91c8d174be0d74cd
Michael.Griffey <Michael.Griffey@e-hps.com>
2018-08-23 09:25:31 -0500

    Added initial version controller

---
+++ Poi.Api/Controllers/VersionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Poi.Api.Controllers
+{
+    [Route("[controller]")]
+    public class VersionController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "1.0.0.0";
+        }
+    }
+}
