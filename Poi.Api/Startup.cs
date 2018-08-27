1cd344ecd051ecf763cb33af6a3a3761c751d170
Michael.Griffey <Michael.Griffey@e-hps.com>
2018-08-27 12:01:13 -0500

    Removed comments

--- Poi.Api/Startup.cs
+++ Poi.Api/Startup.cs
@@ -12,8 +12,6 @@ namespace Poi.Api
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<ICityService, CityService>();
@@ -33,8 +31,7 @@ namespace Poi.Api
                     }
                 });
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
