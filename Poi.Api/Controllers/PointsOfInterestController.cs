db9bd117fe8c6e13daa1758aaf3241eb5f8195ee
Michael.Griffey <Michael.Griffey@e-hps.com>
2018-08-27 13:21:17 -0500

    Removed Unneeded Usings

--- Poi.Api/Controllers/PointsOfInterestController.cs
+++ Poi.Api/Controllers/PointsOfInterestController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Poi.AppServices;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Poi.Api.Controllers
 {
