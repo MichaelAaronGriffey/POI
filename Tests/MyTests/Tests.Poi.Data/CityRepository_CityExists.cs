1dc6ff3ffe84420ebbaa470521c10880e718d57d
Michael.Griffey <Michael.Griffey@e-hps.com>
2018-08-30 15:45:46 -0500

    Tryagain

---
+++ Tests/MyTests/Tests.Poi.Data/CityRepository_CityExists.cs
@@ -0,0 +1,30 @@
+using Xunit;
+using System;
+using Poi.Data.Repositories;
+using Tests.Poi.Data.Data;
+
+namespace Tests.Poi.Data
+{
+    public class CityRepository_CityExists
+    {
+
+        [Theory]
+        [MemberData(nameof(CityGuids.ValidCityGuids), MemberType = typeof(CityGuids))]
+        public void ReturnsTrueForExisting(Guid id)
+        {
+            var context = InMemoryPoiDbContext.GetPoiDbContextSeeded();
+            var repo = new CityRepository(context);
+            Assert.True(repo.CityExists(id));
+        }
+
+        [Theory]
+        [MemberData(nameof(CityGuids.InValidCityGuids), MemberType = typeof(CityGuids))]
+        public void ReturnsFalseForNotExisting(Guid id)
+        {
+            var context = InMemoryPoiDbContext.GetPoiDbContextSeeded();
+            var repo = new CityRepository(context);
+            Assert.False(repo.CityExists(id));
+        }
+    }
+    
+}
---
+++ Tests/MyTests/Tests.Poi.Data/Data/CityGuids.cs
@@ -0,0 +1,19 @@
+﻿using Poi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tests.Poi.Data.Data
+{
+    public static class CityGuids
+    {
+        public static IEnumerable<object[]> ValidCityGuids => PoiDbContextExtension.SeedCities.Select(c => new object[] { c.Id });
+
+        public static IEnumerable<object[]> InValidCityGuids => new List<object[]> {
+            new object[] { Guid.Empty },
+            new object[] { new Guid("11111111-1111-1111-1111-111111111111") }
+        };
+    }
+
+
+}
