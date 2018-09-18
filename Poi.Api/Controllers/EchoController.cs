a080cc7d9a81b89538fa5016a524d456e6bc3171
Michael.Griffey <Michael.Griffey@e-hps.com>
2018-09-18 15:56:08 -0500

    back to the basics

---
+++ Poi.Api/Controllers/EchoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Poi.Api.Controllers
+{
+    [Route("[controller]")]
+    public class EchoController: Controller
+    {
+        [HttpGet("{input}")]
+        public string Get(string input)
+        {
+            return $"Echo:{input}";
+        }
+    }
+}
--- Poi.Api/Startup.cs
+++ Poi.Api/Startup.cs
@@ -27,51 +27,51 @@ namespace Poi.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<ICityService, CityService>();
-            services.AddTransient<ICityRepository, CityRepository>();
+            //services.AddTransient<ICityService, CityService>();
+            //services.AddTransient<ICityRepository, CityRepository>();
 
-            services.AddPoiDbContext(Configuration, Environment);
+            //services.AddPoiDbContext(Configuration, Environment);
 
-            services.AddServicePolicyRegistry();
-            var gitHubUri = Configuration.GetValue<string>("GitHub:uri");
-            services.AddGitHubService(gitHubUri);
+            //services.AddServicePolicyRegistry();
+            //var gitHubUri = Configuration.GetValue<string>("GitHub:uri");
+            //services.AddGitHubService(gitHubUri);
 
-            services.AddPackageInfo();
-            services.AddSwagger();
+            //services.AddPackageInfo();
+            //services.AddSwagger();
 
             services.AddMvc()
-                .AddMvcOptions(o =>
-                {
-                    o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
-                })
-                .AddJsonOptions(o =>
-                {
-                    if (o.SerializerSettings.ContractResolver != null)
-                    {
-                        var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
-                        castedResolver.NamingStrategy = null;
-                    }
-                })
+                //.AddMvcOptions(o =>
+                //{
+                //    o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                //})
+                //.AddJsonOptions(o =>
+                //{
+                //    if (o.SerializerSettings.ContractResolver != null)
+                //    {
+                //        var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+                //        castedResolver.NamingStrategy = null;
+                //    }
+                //})
                 ;
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
-            //loggerFactory.AddNLog();
+            ////loggerFactory.AddNLog();
 
-            Mapper.Initialize(cfg =>
-            {
-                cfg.AddProfile<POIProfile>();
-            });
+            //Mapper.Initialize(cfg =>
+            //{
+            //    cfg.AddProfile<POIProfile>();
+            //});
 
-            app.UseMySwagger();
-            app.UseStaticFiles();
-            app.UseStatusCodePages();
+            //app.UseMySwagger();
+            //app.UseStaticFiles();
+            //app.UseStatusCodePages();
             app.UseMvc();
         }
     }
